6ab10c73553caf92ff133038bca1c00092397063
Prasiddha subedi <prasiddhasubedi592@gmail.com>
2026-01-04 11:53:20 +0545

    Add Firebase configuration file

---
+++ js/firebase-config.js
@@ -0,0 +1,19 @@
+// firebase-config.js
+
+// Replace with your actual Firebase credentials
+const firebaseConfig = {
+  apiKey: "AIzaSyBI9yc2IrLQ_S5KW_doGtXNWX1883un_lw",
+  authDomain: "my-site-92874.firebaseapp.com",
+  projectId: "my-site-92874",
+  storageBucket: "my-site-92874.firebasestorage.app",
+  messagingSenderId: "575232040342",
+  appId: "1:575232040342:web:e4ce6274f43ba320a211b7",
+  measurementId: "G-6CC0FXCQN1", // Optional for Analytics
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+// Export the configuration if needed
+export default firebaseConfig;
