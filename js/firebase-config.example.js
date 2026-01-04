4a6bd9891deacf3f6321bf87940db698864b8bef
Prasiddha subedi <prasiddhasubedi592@gmail.com>
2026-01-04 10:34:28 +0545

    Remove Firebase initialization code and comments
    Removed Firebase initialization and configuration comments.

--- js/firebase-config.example.js
+++ js/firebase-config.example.js
@@ -1,11 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBI9yc2IrLQ_S5KW_doGtXNWX1883un_lw",
   authDomain: "my-site-92874.firebaseapp.com",
@@ -16,6 +8,3 @@ const firebaseConfig = {
   measurementId: "G-6CC0FXCQN1"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
