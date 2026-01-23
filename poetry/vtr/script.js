2e0f87cbc401b657e76d06f95c44cd5be266aaa5
Prasiddha subedi <prasiddhasubedi592@gmail.com>
2026-01-23 11:28:21 +0545

    Delete poetry/vtr directory

--- poetry/vtr/script.js
+++
@@ -1,6 +0,0 @@
-// Poetry page initialization
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Poetry page loaded');
-    
-    // Add any custom animations or interactions here
-});
